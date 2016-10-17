a5f0cc275cec16550e947875d571b2a1ccf65df2
Ram Alapure <alapureram@gmail.com>
2016-10-18 00:20:21 +0530

    Looping and Skipping

--- Bash/LoopingandSkipping.sh
+++ Bash/LoopingandSkipping.sh
@@ -2,3 +2,23 @@ for((x=1; x<100; x+=2));
 do
     echo $x
 done
+
+#or 
+
+seq 1 2 99 # sequence from 1 increment by 2 to 99
+
+
+#or
+
+for i in {1..100..2}
+do
+    echo $i
+done
+
+#or
+
+COUNTER=1
+while [  $COUNTER -lt 100 ]; do
+    echo $COUNTER
+    let COUNTER=COUNTER+2 
+done
